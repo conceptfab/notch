37f2365efe8a51c295f60d4428a26f0f17362b06
CONCEPTFAB <office@conceptfab.com>
2026-05-15 11:17:27 +0200

    fix: bind PreferencesView toggle to @AppStorage

--- NotchShelf/App/PreferencesView.swift
+++ NotchShelf/App/PreferencesView.swift
@@ -2,14 +2,11 @@ import AppKit
 import SwiftUI
 
 struct PreferencesView: View {
-    @State private var copyOnDrag = Preferences.shared.copyOnDrag
+    @AppStorage("copyOnDrag") private var copyOnDrag = false
 
     var body: some View {
         Form {
             Toggle("Zawsze kopiuj pliki przy przeciąganiu z półki", isOn: $copyOnDrag)
-                .onChange(of: copyOnDrag) { _, value in
-                    Preferences.shared.copyOnDrag = value
-                }
 
             Divider()
 
