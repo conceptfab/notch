33017144c33ace9f36b03932c5c9cd2432b33cec
CONCEPTFAB <office@conceptfab.com>
2026-05-23 13:29:40 +0200

    chore: bundle release resources with verification tests

---
+++ NotchShelfTests/BundleResourceTests.swift
@@ -0,0 +1,29 @@
+import AppKit
+import XCTest
+
+final class BundleResourceTests: XCTestCase {
+    func testLicenseTextIsBundled() throws {
+        let url = try XCTUnwrap(
+            Bundle.main.url(forResource: "LICENSE", withExtension: "txt"),
+            "LICENSE.txt must be bundled in the app's Resources"
+        )
+
+        let contents = try String(contentsOf: url, encoding: .utf8)
+        XCTAssertTrue(contents.hasPrefix("MIT License"))
+        XCTAssertTrue(
+            contents.contains("Michal Kleniewski"),
+            "LICENSE.txt must use ASCII author name to match Clank"
+        )
+    }
+
+    func testBuyMeACoffeeImageIsBundled() throws {
+        let url = try XCTUnwrap(
+            Bundle.main.url(forResource: "buy-me-a-coffee", withExtension: "png"),
+            "buy-me-a-coffee.png must be bundled"
+        )
+
+        let image = try XCTUnwrap(NSImage(contentsOf: url), "must be a readable PNG")
+        XCTAssertGreaterThan(image.size.width, 0)
+        XCTAssertGreaterThan(image.size.height, 0)
+    }
+}
